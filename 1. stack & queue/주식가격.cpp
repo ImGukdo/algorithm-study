82a36633842b485b4493c10515feef50029c6417
ImGukdo <85783104+ImGukdo@users.noreply.github.com>
2021-07-12 08:33:17 +0900

    프로그래머스 주식가격 문제

---
+++ 1. stack & queue/주식가격.cpp
@@ -0,0 +1,28 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42584
+// 프로그래머스 주식가격 문제
+// 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 
+// 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 
+// solution 함수를 완성하세요.
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices){
+    vector<int> answer;
+    for(int i = 0; i < prices.size(); ++i){  // 가격을 하나씩 탐색하기 위해 반복문 실행
+        int iCount = 0;  // 하나의 가격이 유지된 초를 기록
+        for(int j = i + 1; j < prices.size(); ++j){  // 현재 위치 이후의 가격과 비교를 위해 반복문 실행
+            if(prices[i] <= prices[j])  // 가격이 떨어지지 않았으면
+                ++iCount;  // 1초 증가 후 반복
+            else{
+                ++iCount;  // 가격이 떨어졌으면 1초 증가 후 반복문 탈출
+                break;
+            }
+        }
+        answer.push_back(iCount);  // 계산된 시간을 저장
+    }
+    
+    return answer;
+}
